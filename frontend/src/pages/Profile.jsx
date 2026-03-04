6611459145bcda8c051936d48073d7f4630bcca6
krupa-shah23 <krupaashah2006@gmail.com>
2026-03-04 11:51:25 +0530

    ui changes

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -5,6 +5,12 @@ import api from "../api/axios";
 import PostCard from "../components/layout/PostCard";
 import PodcastCard from "../components/layout/PodcastCard";
 
+
+
+
+
+
+
 export default function Profile() {
   const { id } = useParams();
   const navigate = useNavigate();
